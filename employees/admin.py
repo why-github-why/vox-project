151171189f5a8465cbeb8278a19c9ebce729691f
Leon Kleinhans <hello@leonkleinhans.com>
2021-07-22 22:26:33 +0200

    minor changes

--- employees/admin.py
+++ employees/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Employee
 
 class EmployeeAdmin(admin.ModelAdmin):
-   list_display = ('id', 'username', 'email', 'date_registered')
+   list_display = ('id', 'username', 'email', 'phone', 'date_registered')
    list_display_links = ('id', 'username')
    search_fields = ('username', 'email')
    list_per_page = 25
